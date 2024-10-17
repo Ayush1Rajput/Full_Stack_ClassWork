669abdad2cccd9cbdd70f55a478d23c89b9268fa
Ayush1Rajput <ayushraj1rajput@gmail.com>
2024-10-17 11:54:15 +0530

    JS file

--- JavaScript/L-7/script.js
+++ JavaScript/L-7/script.js
@@ -1,22 +1,25 @@
-// setTimeout(() => {
-//   console.log("Hello after 6 Sec.");
-// }, 6000);
+setTimeout(() => {
+  console.log("Hello after 6 Sec.");
+}, 6000);
 
-// console.log("Mid");
+console.log("Mid");
 
-// setTimeout(() => {
-//   console.log("Hello after 4 Sec.");
-// }, 4000);
+setTimeout(() => {
+  console.log("Hello after 4 Sec.");
+}, 4000);
 
+setTimeout(() => {
+  console.log("Hello after 7 Sec.");
+}, 7000);
 // let date = new Date();
 
 // console.log(date);
 // console.log(date.getTime());
 
-console.log("start");
+// console.log("start");
 
 let d = new Date().getTime();
 
-while (new Date().getTime() < d + 5000) {}
+while (new Date().getTime() < d + 8000) {}
 
 console.log("End");
