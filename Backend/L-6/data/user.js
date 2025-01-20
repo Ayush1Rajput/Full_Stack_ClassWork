635c74e4b2ca048f7b332418749835401b27c822
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-01-20 12:50:35 +0530

    Creating method-override delete post

---
+++ Backend/L-6/data/user.js
@@ -0,0 +1,179 @@
+let Users = [
+  {
+    "id": 1,
+    "username": "johndoe",
+    "email": "johndoe@example.com",
+    "password": "password123",
+    "city": "New York"
+  },
+  {
+    "id": 2,
+    "username": "janedoe",
+    "email": "janedoe@example.com",
+    "password": "janedoe2025",
+    "city": "Los Angeles"
+  },
+  {
+    "id": 3,
+    "username": "alice_smith",
+    "email": "alice.smith@example.com",
+    "password": "alice2024",
+    "city": "Chicago"
+  },
+  {
+    "id": 4,
+    "username": "bob_jones",
+    "email": "bob.jones@example.com",
+    "password": "bob2025",
+    "city": "San Francisco"
+  },
+  {
+    "id": 5,
+    "username": "emilybrown",
+    "email": "emily.brown@example.com",
+    "password": "emily123",
+    "city": "Houston"
+  },
+  {
+    "id": 6,
+    "username": "charlie_williams",
+    "email": "charlie.williams@example.com",
+    "password": "charlie2025",
+    "city": "Miami"
+  },
+  {
+    "id": 7,
+    "username": "david_miller",
+    "email": "david.miller@example.com",
+    "password": "david2024",
+    "city": "Dallas"
+  },
+  {
+    "id": 8,
+    "username": "sarah_lee",
+    "email": "sarah.lee@example.com",
+    "password": "sarah1234",
+    "city": "Seattle"
+  },
+  {
+    "id": 9,
+    "username": "michael_clark",
+    "email": "michael.clark@example.com",
+    "password": "michael2025",
+    "city": "Denver"
+  },
+  {
+    "id": 10,
+    "username": "lisa_white",
+    "email": "lisa.white@example.com",
+    "password": "lisa2025",
+    "city": "Austin"
+  },
+  {
+    "id": 11,
+    "username": "kevin_lee",
+    "email": "kevin.lee@example.com",
+    "password": "kevin2025",
+    "city": "Boston"
+  },
+  {
+    "id": 12,
+    "username": "jennifer_king",
+    "email": "jennifer.king@example.com",
+    "password": "jennifer2025",
+    "city": "Washington D.C."
+  },
+  {
+    "id": 13,
+    "username": "olivia_brown",
+    "email": "olivia.brown@example.com",
+    "password": "olivia123",
+    "city": "Phoenix"
+  },
+  {
+    "id": 14,
+    "username": "william_morris",
+    "email": "william.morris@example.com",
+    "password": "william123",
+    "city": "Orlando"
+  },
+  {
+    "id": 15,
+    "username": "lucas_hall",
+    "email": "lucas.hall@example.com",
+    "password": "lucas2025",
+    "city": "Atlanta"
+  },
+  {
+    "id": 16,
+    "username": "mia_evans",
+    "email": "mia.evans@example.com",
+    "password": "mia2025",
+    "city": "Phoenix"
+  },
+  {
+    "id": 17,
+    "username": "james_williams",
+    "email": "james.williams@example.com",
+    "password": "james2025",
+    "city": "San Diego"
+  },
+  {
+    "id": 18,
+    "username": "natalie_smith",
+    "email": "natalie.smith@example.com",
+    "password": "natalie123",
+    "city": "Las Vegas"
+  },
+  {
+    "id": 19,
+    "username": "noah_johnson",
+    "email": "noah.johnson@example.com",
+    "password": "noah2025",
+    "city": "Portland"
+  },
+  {
+    "id": 20,
+    "username": "emma_wilson",
+    "email": "emma.wilson@example.com",
+    "password": "emma2025",
+    "city": "Salt Lake City"
+  },
+  {
+    "id": 21,
+    "username": "ava_davis",
+    "email": "ava.davis@example.com",
+    "password": "ava1234",
+    "city": "Minneapolis"
+  },
+  {
+    "id": 22,
+    "username": "liam_rodgers",
+    "email": "liam.rodgers@example.com",
+    "password": "liam2025",
+    "city": "Chicago"
+  },
+  {
+    "id": 23,
+    "username": "sofia_wood",
+    "email": "sofia.wood@example.com",
+    "password": "sofia2025",
+    "city": "Boston"
+  },
+  {
+    "id": 24,
+    "username": "lucy_taylor",
+    "email": "lucy.taylor@example.com",
+    "password": "lucy2025",
+    "city": "Los Angeles"
+  },
+  {
+    "id": 25,
+    "username": "jack_jonas",
+    "email": "jack.jonas@example.com",
+    "password": "jack2025",
+    "city": "Seattle"
+  }
+];
+
+module.exports = Users;
---
+++ Backend/L-6/index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const app = express();
+const path = require("path")
+const Users = require('./data/user');
+const methodOverride = require('method-override')
+
+
+app.use(methodOverride('_method'))
+
+
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({extended:true}));
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
+
+app.get('/',(req,res)=>{
+    res.send('HOME PAGE')
+})
+
+app.get('/users',(req,res)=>{
+    res.render('user',{Users})
+})
+
+app.get('/user/new',(req,res)=>{
+    res.render('new')
+})
+
+app.post('/users',(req,res)=>{
+    const {username,password,email,city} = req.body;
+    let id = Users[Users.length-1].id+1;
+    let user = {id,username,password,email,city};
+    Users.push(user);
+    res.redirect('/users')
+})
+
+app.get('/users/:id',(req,res)=>{
+    const {id} = req.params;
+
+    let user = Users.find((user)=>user.id==id);
+
+    res.render('show',{user});
+
+})
+
+app.get('/users/:id/edit',(req,res)=>{
+    const {id} = req.params;
+    let user = Users.find((user)=>user.id==id);
+
+    res.render('edit',{user});
+})
+
+app.post('/abc/:id',(req,res)=>{
+    const {id} = req.params; 
+    let user = Users.find((user)=>user.id==id);
+    const {username,password,email,city} = req.body;
+    user.username = username;
+    user.password = password;
+    user.city = city;
+    user.email = email;
+
+    res.redirect('/users')
+})
+
+// app.post('/users/:id/delete',(req,res)=>{
+//     const {id} = req.params;
+//     let user = Users.find((user)=>user.id==id);
+//     let idx = Users.indexOf(user);
+//     Users.splice(idx,1);
+    
+//     res.redirect('/users');
+// })
+
+app.delete('/users/:id/',(req,res)=>{
+    const {id} = req.params;
+    let user = Users.find((user)=>user.id==id);
+    let idx = Users.indexOf(user);
+    Users.splice(idx,1);
+    
+    res.redirect('/users');
+})
+
+app.listen(4040,()=>{
+    console.log('server running at port 4040');
+})
