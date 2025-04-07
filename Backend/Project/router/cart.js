2cd444389e3e7e3c1fc106ca28dd3db1229f1a83
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-04-07 12:56:31 +0530

    Developing the Cart part

--- Backend/Project/router/cart.js
+++ Backend/Project/router/cart.js
@@ -1,9 +1,7 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const Product = require('../model/product');
 const User = require('../model/user')
-const Product = require('../model/product')
-
-
 
 router.get('/users/cart/add/:productId', async (req, res) => {
     const { productId } = req.params;
@@ -11,17 +9,10 @@ router.get('/users/cart/add/:productId', async (req, res) => {
 
     const userId = req.user._id;
     const user = await User.findById(userId);
-
-    // let idx = User.cart.findIndex(p => p === productId)
-    // if (idx !== -1) {
-    //     console.log("No")
-    // } else {
-    //     console.log("Yes");
-    // }
-
     let index = -1;
+
     let x = user.cart.find((item, ind) => {
-        if (item.productId === productId) {
+        if (item.productId == productId) {
             index = ind;
             return item;
         }
@@ -29,23 +20,22 @@ router.get('/users/cart/add/:productId', async (req, res) => {
 
     if (x) {
         user.cart[index].quantity += 1;
-    } else {
-        user.cart.push({ productId });
+    }
+    else {
+        user.cart.push({ productId })
     }
 
-    await user.save()
+    await user.save();
 
-    res.redirect('/products');
+    res.redirect('back')
+
 })
 
 router.get('/users/cart', async (req, res) => {
-
     const userId = req.user._id;
-    const user = await User.findById(userId).populate(User.productId);
+    const user = await User.findById(userId).populate('cart.productId');
 
-    // console.log(user.productId)
-    res.render('cart/index', { user })
-
+    res.render('cart/index', { cart: user.cart })
 })
 
 module.exports = router;
